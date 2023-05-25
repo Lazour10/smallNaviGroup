ef1f0024e28a492a81172501300a8692c9397b59
Lazour10 <lazour@my.waketech.edu>
2023-05-25 13:29:16 -0400

    set up first Practice

---
+++ src/FirstPractice/SumArray.java
@@ -0,0 +1,26 @@
+package FirstPractice;
+
+/*
+this method accepts an Array and returns the sum of all the elements in the array
+ */
+
+    public class SumArray {
+
+        public static void main(String[] args) {
+
+            int[] abc = {1,2,3,4,5,6};
+
+            int ab = Sum(abc);
+
+            System.out.println(ab);
+        }
+
+        public static int Sum(int[]a){
+            int result = 0;
+            for (int i = 0; i < a.length; i++) {
+                result += a[i]; //increment
+            }
+            return result;
+        }
+    }
+
